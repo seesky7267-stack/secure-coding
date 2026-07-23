976fb57e14bafb2af3aa4a117f9e3c4c211956f1
user1 <seesky7267@gmail.com>
2026-07-24 05:38:39 +0900

    Add login page

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "중고거래 플랫폼 서버가 정상적으로 실행되었습니다."
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    return f"입력된 아이디: {username}, 비밀번호: {password}"
+
+
+@app.route("/register")
+def register():
+    return "회원가입 페이지는 다음 단계에서 만듭니다."
 
 
 if __name__ == "__main__":
